ff4a4ef8f60c89adbc1c82fd08ee2d9ec6f7ca9d
Matt Remick <mremick@gmail.com>
2014-02-24 17:56:18 -0800

    Done!

--- LinkedListAndBinarySearchTree/ViewController.m
+++ LinkedListAndBinarySearchTree/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "Node.h"
 #import "LinkedList.h"
+#import "BinaryTree.h"
 
 @interface ViewController ()
 
@@ -20,6 +21,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+//    [self linkedList];
+    [self binaryTree];
+    
+    
+}
+
+
+- (void) linkedList
+{
     
     LinkedList *list = [[LinkedList alloc] init];
     
@@ -27,15 +37,31 @@
     for (int i = 0; i < 20; i++) {
         Node *node = [[Node alloc] initWithData:i];
         [list push:node];
-
+        
     }
     
     [list print];
-
+    
     Node *returnedNode = [list searchForNode:35];
     [returnedNode printNode];
-    
 }
+
+     
+- (void) binaryTree
+{
+    BinaryTree * tree = [[BinaryTree alloc] init];
+    
+    for (int i = 0; i < 20; i++) {
+        [tree addNodeWithValue: arc4random() % 20];
+    }
+    
+    [tree print];
+    
+    NSLog(@"Search Result:\n");
+    TreeNode *returnedNode = [tree searchValue:5];
+    [returnedNode printNode];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
